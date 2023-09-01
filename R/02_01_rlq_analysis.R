66b2a64a58ef5bd6e25a50f2db784746d43245e7
Fanfoué <francois.martin74190@gmail.com>
2023-09-01 17:32:34 +0200

    :white_check_mark: Improved formatting

--- R/02_01_rlq_analysis.R
+++ R/02_01_rlq_analysis.R
@@ -16,9 +16,17 @@
 # ----------------------- #
 ##### 1. RLQ analysis #####
 # ----------------------- #
-
+# *---------------------------------------------------* #####
 # ---------------------------------------------------------------------------- #
 ##### * 1.1. Preliminary two-tables analyses -----------------------------------
 # ---------------------------------------------------------------------------- #
+#
+# # A preliminary step of RLQ analysis is to perform the separate analyses of each table. Because of the two
+# tables associated with the two margins of the L table (L = link; the site x species table), the L table has
+# to be symmetric and should thus be transformed using a Correspondence Analysis.
+
+#
 ##### ** 1.1.1. XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ----
 # __________________________________________________
+
+
